3fe2eb0055790029f9a90144434bab05a5b98de4
Prayag Desai <15894048+prayag2010@users.noreply.github.com>
2018-07-19 23:38:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -127,12 +127,12 @@ app.get('/light1/', logRequest, function (req, res) {
     res.sendStatus(200);
 });
 
-// app.post('/post/', logRequest, authorizeUser, checkSingleFileUpload, checkMessagePathQueryParameter, checkTopicQueryParameter, ensureTopicSpecified, function (req, res) {
-//     mqttClient.publish(req.body['topic'], req.body['message']);
-//     res.sendStatus(200);
-// });
+app.post('/post/', logRequest, authorizeUser, checkSingleFileUpload, checkMessagePathQueryParameter, checkTopicQueryParameter, ensureTopicSpecified, function (req, res) {
+    mqttClient.publish(req.body['topic'], req.body['message']);
+    res.sendStatus(200);
+});
 
-app.post('/post/', logRequest, function (req, res) {
+app.post('/dialog/', logRequest, function (req, res) {
     mqttClient.publish('test', req.body.result.parameters['color']);
     res.sendStatus(200);
 });
