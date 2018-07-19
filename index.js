602ee2bcd4e432ef03a0ca7ebba788f7101829d5
Prayag Desai <15894048+prayag2010@users.noreply.github.com>
2018-07-19 23:33:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ app.get('/light1/', logRequest, function (req, res) {
 // });
 
 app.post('/post/', logRequest, function (req, res) {
-    mqttClient.publish('test', req.result['source']);
+    mqttClient.publish('test', req.body.result['source']);
     res.sendStatus(200);
 });
 
